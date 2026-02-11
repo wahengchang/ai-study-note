b515b594d4f394414b8ceafb76e9e88b4d885ea6
waheng chang <jy02927731@gmail.com>
2026-02-11 11:14:05 +0800

    updated repo link

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "wahengchang.github.io/quartz-note",
+    baseUrl: "wahengchang.github.io/ai-study-note",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
