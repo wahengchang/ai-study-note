0f39fb6c432c181d0b3db99efb773ad77b6e144c
ppchang <wahengchang2023@gmail.com>
2026-04-03 01:02:10 +0800

    updated style

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,7 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [Component.Graph()],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/wahengchang/ai-study-note",
@@ -57,7 +57,6 @@ export const defaultContentPageLayout: PageLayout = {
   right: [
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
-    Component.Graph(),
   ],
 }
 
